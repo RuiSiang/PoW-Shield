8ac33d71e497a977a323aac0c4f22d4a11633641
RuiSiang <49300088+RuiSiang@users.noreply.github.com>
2021-03-10 09:59:41 +0800

    fix ci problem and revert koa-views version

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,8 @@
 #!bin/bash
-tsc
-cp -rfRT views dist/views
-cp -rfRT public dist/public
-cp package.json dist/package.json
-cp config.js dist/config.js
+tsc && \
+echo "asdf" && \
+cp -rfRT views dist/views && \
+cp -rfRT public dist/public && \
+cp package.json dist/package.json && \
+cp config.js dist/config.js && \
 browserify lib/bundle.js -t uglifyify | uglifyjs > dist/public/javascripts/bundle.min.js
