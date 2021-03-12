3334614470d81a40a587aecf83cd484cda635509
RuiSiang <49300088+RuiSiang@users.noreply.github.com>
2021-03-12 10:56:53 +0800

    change config.js to .env

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,12 @@
 #!bin/bash
-cp config.example.js config.js && \
+cp -n .env.example .env && \
+echo "Starting Build..." && \
 tsc && \
-echo "asdf" && \
+echo "Build Successful!" && \
+echo "Copying Files..." && \
 cp -rfRT views dist/views && \
 cp -rfRT public dist/public && \
 cp package.json dist/package.json && \
-cp config.js dist/config.js && \
-browserify lib/bundle.js -t uglifyify | uglifyjs > dist/public/javascripts/bundle.min.js
+cp .env dist/.env && \
+browserify lib/bundle.js -t uglifyify | uglifyjs > dist/public/javascripts/bundle.min.js && \
+echo "Success!"
